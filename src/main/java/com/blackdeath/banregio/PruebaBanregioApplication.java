13dac940f3b4856c02fad48ec7c052c489cf5c00
Seth Karim Luis Martínez <seth.luis@banregio.com>
2025-08-12 18:21:19 -0600

    Primer commit

---
+++ src/main/java/com/blackdeath/banregio/PruebaBanregioApplication.java
@@ -0,0 +1,13 @@
+package com.blackdeath.banregio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaBanregioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaBanregioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackdeath/banregio/PruebaBanregioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackdeath.banregio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaBanregioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
